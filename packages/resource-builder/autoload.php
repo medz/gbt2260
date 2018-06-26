7f2f03c3812df5527df817ddb5b0b1b255167710
Seven Du <shiweidu@outlook.com>
2018-06-27 02:31:55 +0800

    chore(2.0, Builder): 增加想尽注释

--- packages/resource-builder/autoload.php
+++ packages/resource-builder/autoload.php
@@ -1,11 +1,15 @@
 <?php
 
+// Defined all autoload files.
 $autoloadFiles = [
 	__DIR__.'/../../vendor/autoload.php',
 	__DIR__.'/../../../../vendor/autoload.php',
 ];
+
+// is autoload file loaded.
 $autoloadFileLoaded = false;
 
+// include autoload files.
 foreach ($autoloadFiles as $filename) {
 	if (file_exists($filename)) {
 		include $filename;;
@@ -13,7 +17,10 @@ foreach ($autoloadFiles as $filename) {
 	}
 }
 
+// Not Fount autoload file.
 if (!$autoloadFileLoaded) {
 	echo 'Not fount autoload file.', PHP_EOL;
+
+	// Send exit error code.
 	exit(1);
 }
