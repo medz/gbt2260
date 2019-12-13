fe7e6be2f98703120d025dfc031c65b7b6509dc6
Seven Du <shiweidu@outlook.com>
2019-12-14 02:54:19 +0800

    chore: 增加相关注释

--- php/Getter.php
+++ php/Getter.php
@@ -6,11 +6,29 @@ namespace Medz\GBT2260;
 
 class Getter
 {
+    /**
+     * 当前 resource 的年份
+     */
     static protected $year;
+
+    /**
+     * 最新的 resource 副本
+     */
     static protected $resource;
+
+    /**
+     * 年份 Diff 缓存
+     */
     static protected $cache;
+
+    /**
+     * 当前实例
+     */
     static protected $instance;
 
+    /**
+     * 创建当前实例
+     */
     private function __construct()
     {
         $alias = file_get_contents(realpath(__DIR__.'/../resources/last.alias'));
@@ -18,6 +36,9 @@ class Getter
         static::$resource = json_decode(file_get_contents(realpath(__DIR__.'/../resources/'.$alias.'.json')), true);
     }
 
+    /**
+     * 获取实例
+     */
     static public function instance(): Getter
     {
         if (static::$instance instanceof Getter) {
@@ -27,6 +48,9 @@ class Getter
         return static::$instance = new Getter;
     }
 
+    /**
+     * 获取省份字符串
+     */
     public function province(string $code): ?string
     {
         $provinceCode = substr($code, 0, 2).'0000';
@@ -38,6 +62,9 @@ class Getter
         return $this->_previousGetter(static::$year, static::$resource, $provinceCode);
     }
 
+    /**
+     * 获取城市字符串
+     */
     public function city(string $code): ?string
     {
         $cityCode = substr($code, 0, 4).'00';
@@ -48,6 +75,9 @@ class Getter
         return $this->_previousGetter(static::$year, static::$resource, $cityCode);
     }
 
+    /**
+     * 获取地区字符串
+     */
     public function county(string $code): ?string
     {
         if (isset(static::$resource[$code])) {
@@ -57,6 +87,9 @@ class Getter
         return $this->_previousGetter(static::$year, static::$resource, $code);
     }
 
+    /**
+     * 递归历代年份日志
+     */
     protected function _previousGetter($year, array $resource, string $code): ?string
     {
         if ($year == 1980) {
@@ -80,6 +113,9 @@ class Getter
         return $this->_previousGetter((string) ($year - 1), (array) $resource, (string) $code);
     }
 
+    /**
+     * 获取并缓存对比日志
+     */
     protected function _getCache($year) {
         $key = "{$year}-previous";
         if (!isset(static::$cache[$key])) {
