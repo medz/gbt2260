45ea8c55452b0079d931da60ec035e734c627ba0
Seven Du <whine.whitish.0u@icloud.com>
2022-07-29 14:50:14 +0800

    chore 重新整理 1981 年数据，修复纰漏数据

--- build.dart
+++ build.dart
@@ -2,62 +2,67 @@ import 'dart:convert';
 import 'dart:io';
 
 void main() async {
-  /// Resource format:
-  /// {
-  ///   "110000": "北京市",
-  // "110101": "东城区",
-  // "110102": "西城区",
-  // "110103": "崇文区",
-  // "110104": "宣武区",
-  // "110105": "朝阳区",
-  // "110106": "丰台区",
-  // "110107": "石景山区",
-  // "110108": "海淀区",
-  // "110109": "门头沟区",
-  // "110110": "燕山区",
-  // "110201": "昌平县",
-  // "110202": "顺义县",
-  // "110203": "通县",
-  // "110204": "大兴县",
-  // "110205": "房山县",
-  // "110206": "平谷县",
-  // "110207": "怀柔县",
-  // "110208": "密云县",
-  // "110209": "延庆县",
-  // "120000": "天津市",
-  // "120101": "和平区",
-  // "120102": "河东区",
-  // "120103": "河西区",
-  // "120104": "南开区",
-  /// }
-  final source = readResopurce();
+  final last = readResopurce('./resources/1981.json');
+  final first = readResopurce('./resources/1980.json');
 
-  // 使用 source 创造一个 kv 文件，并保存到本地
-  // kv 文件格式为：
-  // 11 <> 北京市
-  // 110101 <> 东城区
-  // 110102 <> 西城区
-  // 110103 <> 崇文区
-  final kv =
-      source.map((k, v) => MapEntry(k, '${removeEndZero(k)} <> $v')).values;
+  // last 相较于 first
+  Map added = {};
+  Map nameChanged = {};
+  for (var element in last.entries) {
+    if (first.containsKey(element.key) && last.containsKey(element.key)) {
+      if (first[element.key] != last[element.key]) {
+        nameChanged[first[element.key]] = last[element.key];
+        continue;
+      }
+    }
 
-  final file = File('data.kv');
-  await file.writeAsString(kv.join('\n'));
-}
-
-// Read '1980.json' and parse it as a JSON object.
-Map<String, dynamic> readResopurce() {
-  final File file = File('resources/1980.json');
-
-  return json.decode(file.readAsStringSync());
-}
-
-// Remove string end '0' from a string.
-// E.g: 110000 -> 11
-String removeEndZero(String str) {
-  if (str.endsWith('0')) {
-    return removeEndZero(str.substring(0, str.length - 1));
+    if (!first.containsKey(element.key)) {
+      added[element.key] = element.value;
+    }
   }
 
-  return str;
+  final removed = {};
+  for (var element in first.entries) {
+    if (!last.containsKey(element.key)) {
+      removed[element.key] = element.value;
+    }
+  }
+
+  // 写入到 diff 文件
+  final file = File('./diff.txt');
+  String changedString = '';
+  String addedString = '';
+  String removedString = '';
+
+  for (var element in nameChanged.entries) {
+    changedString += '${element.key} > ${element.value}\n';
+  }
+
+  for (var element in added.entries) {
+    addedString += '${element.key} <> ${element.value}\n';
+  }
+
+  for (var element in removed.entries) {
+    removedString += '${element.key} <> ${element.value}\n';
+  }
+
+  await file.writeAsString('''
+改名的：
+$changedString
+==============
+新增的：
+$addedString
+==============
+移除的：
+$removedString
+''');
+}
+
+// Read file and parse it as a JSON object.
+Map<String, dynamic> readResopurce(String path) {
+  final File file = File(path);
+
+  // return {};
+
+  return json.decode(file.readAsStringSync());
 }
